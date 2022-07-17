87cf183f34ce0855ef528d364237558b8cbdf4cb
Detril <andremoukarzel@gmail.com>
2022-07-17 07:25:29 -0300

    Fixing knight movement

--- enemies/knight_movement_script.gd
+++ enemies/knight_movement_script.gd
@@ -15,9 +15,6 @@ func move(direction: Vector2i) -> Tween:
 	mov_tween.chain().tween_property(self, "position", Vector2(get_absolute_pos()), MOV_TIME/2)
 	
 	mov_tween.chain().tween_property($Sprite2D, "scale", Vector2(1, 1), MOV_TIME)
-	
-	await mov_tween.finished
-	$MoveSfx.play()
 
 	return mov_tween
 
@@ -83,3 +80,24 @@ func getPathToPlayer(player_pos: Vector2i, obstacles_positions=[]):
 	var player_pos_id = getIdFromCoordinates(player_pos.x,  player_pos.y)
 	var path = astar.get_point_path(enemy_pos_id, player_pos_id)
 	return(path)
+	
+func act(player_pos: Vector2i, obstacles: Array):
+	if is_frozen:
+		is_frozen = false
+	else:
+		unfreeze()
+		getAvailablePoints(map_rows, map_cols, obstacles)
+		var path_coordinates = getPathToPlayer(player_pos)
+		var moves_made = min(mov_num, len(path_coordinates)-1)
+		
+		var movements_to_be_made = []
+		var x_mov = 0
+		var y_mov = 0
+		
+		for i in range(1, moves_made+1):
+			x_mov = path_coordinates[i].x - path_coordinates[i-1].x
+			y_mov = path_coordinates[i].y - path_coordinates[i-1].y
+			movements_to_be_made.append(Vector2i(x_mov, y_mov))
+		
+		for movement in movements_to_be_made:
+			var twn = move(movement)
