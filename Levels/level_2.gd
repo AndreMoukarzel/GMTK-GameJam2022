8a30b0146fd639ddce7e124dbdba94f784ef5d1e
Andre Ferrari <andremoukarzel@gmail.com>
2022-07-17 03:28:28 -0300

    Made levels smaller

--- Levels/level_2.gd
+++ Levels/level_2.gd
@@ -3,7 +3,7 @@ extends "res://Levels/LevelTemplate.gd"
 # Called when the node enters the scene tree for the first time.
 func update_values():
 	level = 2
-	MAP_SIZE = Vector2i(42, 31)
+	MAP_SIZE = Vector2i(29, 22)
 	wall_positions = []
 	enemies = [
 		["base_enemy", Vector2i(1, 5)],
